92a857c55af64d4ceeffb4413730b2de93eabbdd
Bikcraft <felipebaiano04@gmail.com>
2023-09-27 16:44:15 -0300

    Alert erro adicionado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import { AlertError } from "./alert-error.js";
 import { Modal } from "./modal.js";
 
 const form = document.querySelector("form");
@@ -10,10 +11,18 @@ function notRecharge(event) {
   event.preventDefault();
 }
 
-form.onsubmit = () => {
+form.onsubmit = (event) => {
   notRecharge(event);
   const weight = inputWeight.value;
   const height = inputHeight.value;
+
+  const showAlertError = notNumber(weight) || notNumber(height);
+
+  if (showAlertError) {
+    AlertError.open();
+    return;
+  }
+  AlertError.close();
 
   const result = IMC(weight, height);
   let messege = `Seu imc e ${result}`;
@@ -23,6 +32,10 @@ form.onsubmit = () => {
   //modalWrapper.classList.add("open");
   Modal.open();
 };
+
+function notNumber(value) {
+  return isNaN(value) || value == ("", ",", ".");
+}
 
 function IMC(weight, height) {
   return (weight / (height / 100) ** 2).toFixed(2);
